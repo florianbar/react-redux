d1063c9948b0cd35e21395a9307f3331f3fe1abf
florianbar <florianbar01@gmail.com>
2020-08-18 08:52:15 +0200

    Setup Redux example

---
+++ redux-basics.js
@@ -0,0 +1,22 @@
+// To execute code in terminal:
+// node redux-basics.js
+
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+};
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+    return state;
+};
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+
+// Subscription
