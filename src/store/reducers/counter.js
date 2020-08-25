f08b62e2c3c1354a9adb616f2bdf202aa43ba341
florianbar <florianbar01@gmail.com>
2020-08-25 19:35:34 +0200

    Added Utility function to update state object in reducer

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const counterState = {
     counter: 0
@@ -7,28 +8,28 @@ const counterState = {
 const counterReducer = (state = counterState, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1
-            }
+            return updateObject(
+                state, 
+                { counter: state.counter + 1 }
+            );
 
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
+            return updateObject(
+                state, 
+                { counter: state.counter - 1 }
+            );
 
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
+            return updateObject(
+                state, 
+                { counter: state.counter + action.val }
+            );
 
         case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
+            return updateObject(
+                state, 
+                { counter: state.counter - action.val }
+            );
 
         default:
             return state;
--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const resultsState = {
     results: []
@@ -7,20 +8,20 @@ const resultsState = {
 const resultsReducer = (state = resultsState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: action.result })
-            }
+            return updateObject(
+                state, 
+                { results: state.results.concat({ id: new Date(), value: action.result }) }
+            );
 
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];
             // newArray.results.splice(id, 1);
             const updatedArray = state.results.filter(result => result.id !== action.resultId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(
+                state, 
+                { results: updatedArray }
+            );
 
         default:
             return state;
