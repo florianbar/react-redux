512812a87907e07fb9ec491b24b75afc288459ac
florianbar <florianbar01@gmail.com>
2020-08-25 19:40:44 +0200

    Leaner reducer switch case statements

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -8,29 +8,17 @@ const counterState = {
 const counterReducer = (state = counterState, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            return updateObject(
-                state, 
-                { counter: state.counter + 1 }
-            );
+            return updateObject(state, { counter: state.counter + 1 });
 
         case actionTypes.DECREMENT:
-            return updateObject(
-                state, 
-                { counter: state.counter - 1 }
-            );
+            return updateObject(state, { counter: state.counter - 1 });
 
         case actionTypes.ADD:
-            return updateObject(
-                state, 
-                { counter: state.counter + action.val }
-            );
+            return updateObject(state, { counter: state.counter + action.val });
 
         case actionTypes.SUBTRACT:
-            return updateObject(
-                state, 
-                { counter: state.counter - action.val }
-            );
-
+            return updateObject(state, { counter: state.counter - action.val });
+            
         default:
             return state;
     }
--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -5,23 +5,21 @@ const resultsState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    // const id = 2;
+    // const newArray = [...state.results];
+    // newArray.results.splice(id, 1);
+    const updatedArray = state.results.filter(result => result.id !== action.resultId);
+    return updateObject(state, { results: updatedArray });
+};
+
 const resultsReducer = (state = resultsState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return updateObject(
-                state, 
-                { results: state.results.concat({ id: new Date(), value: action.result }) }
-            );
-
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
+        
         case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.results.splice(id, 1);
-            const updatedArray = state.results.filter(result => result.id !== action.resultId);
-            return updateObject(
-                state, 
-                { results: updatedArray }
-            );
+            return deleteResult(state, action);
 
         default:
             return state;
